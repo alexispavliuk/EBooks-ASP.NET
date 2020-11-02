70f61a3e07d142d9cdedbdc17be3d43e7bd861e4
OleksiiPavliuk <alexispavliuk@gmail.com>
2020-11-02 15:35:12 +0200

    add bundles and static files

--- EBooks-ASP.NET/App_Start/BundleConfig.cs
+++ EBooks-ASP.NET/App_Start/BundleConfig.cs
@@ -14,16 +14,10 @@ namespace EBooks_ASP.NET
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            // Используйте версию Modernizr для разработчиков, чтобы учиться работать. Когда вы будете готовы перейти к работе,
-            // готово к выпуску, используйте средство сборки по адресу https://modernizr.com, чтобы выбрать только необходимые тесты.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/script").Include(
+            "~/Scripts/script.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
                       "~/Content/site.css"));
         }
     }
