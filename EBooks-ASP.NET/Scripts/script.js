1e42e6608c87ea8e712bcf5cf7082100dc4747b5
OleksiiPavliuk <alexispavliuk@gmail.com>
2020-11-04 15:17:21 +0200

    add js script

---
+++ EBooks-ASP.NET/Scripts/script.js
@@ -0,0 +1,19 @@
+﻿window.onload = function () {
+    document.getElementById("up_button").addEventListener("click", topFunction);
+    document.getElementById("lang_panel").addEventListener("click", chooseLang);
+    document.querySelectorAll("p.lang")[0].style.color = "#4e6cbc";
+}
+
+function topFunction() {
+    document.documentElement.scrollTop = 0;
+    document.body.scrollTop = 0;
+}
+
+function chooseLang(e) {
+    if (e.target.className != "lang") {
+        return;
+    }
+    let languges = document.querySelectorAll("p.lang");
+    languges.forEach(lang => lang.style.color = "#a4a9ba");
+    e.target.style.color = "#4e6cbc";
+}
